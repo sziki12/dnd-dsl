18eacdd3cd384d4004a46464529d570ad3ed406f
sziki12 <szikilevi01@gmail.com>
2026-04-18 00:27:02 +0200

    Parsing investigation

--- dnd-dsl-backend/src/langium-interpreter/langium-interpreter.service.ts
+++ dnd-dsl-backend/src/langium-interpreter/langium-interpreter.service.ts
@@ -1,4 +1,4 @@
-import { Model } from '@dnd-language/index.js';
+import { Code, CodeBlock, Expression, isConstantExpression, Model, ReferenceExpression } from '@dnd-language/index.js';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -30,4 +30,64 @@ export class LangiumInterpreterService {
         }
         return current;
     }
+
+    evaluateExpression(worldState: any, expression: Expression): any {
+
+        if(isConstantExpression(expression)){
+            if(expression.$type === "IntVal" || expression.$type === "BoolVal" || expression.$type === "StringVal"){
+                return expression.val;
+            }
+
+            if(expression.$type === "ObjectDeclaration"){
+                return expression.variables.reduce((obj: any, variable) => {
+                    obj[variable.target!] = this.evaluateExpression(worldState, variable.value!);
+                    return obj;
+                }, {});
+            }
+        }
+
+        if(expression.$type === "GroupedExpression"){
+            return `(${this.evaluateExpression(worldState, expression.exp)})`;
+        } 
+    }
+
+    evaluateReference(worldState: any, expression: ReferenceExpression): any {
+        if(expression.$type === "RefChain"){
+            return expression.first;
+            //TODO
+        }
+    }
+
+    
+    callFunction(params: callFunctionParams): any {
+
+    }
+
+    runCodeBlock(worldState: any, codeBlock: CodeBlock): any {
+        codeBlock.code.forEach(code => {
+            this.runCode(worldState, code);
+        });
+    }
+
+    runCode(worldState: any, code: Code): any {
+        if(code.$type === "FunctionCall"){
+            return this.callFunction({
+                worldState: worldState,
+                target: code.target!.val.ref!.name,
+                type: "name"
+            });
+        }
+
+        if(code.$type === "VariableAssignment"){
+            const value = this.evaluateExpression(worldState, code.value);
+            const variableName = code.target.val.ref!.name!;
+            worldState[variableName] = value;
+        }
+    }
 }
+
+type callFunctionParams = {
+        worldState: any,
+        target: string,
+        type?: "reference" | "name",
+    }
