fd625b9122c5f361986b209b495ac5b4eeeb29a4
sziki12 <szikilevi01@gmail.com>
2026-05-19 17:09:24 +0200

    Expression evaluation and code running

--- dnd-dsl-backend/src/langium-interpreter/langium-interpreter.service.ts
+++ dnd-dsl-backend/src/langium-interpreter/langium-interpreter.service.ts
@@ -1,66 +1,183 @@
-import { Code, CodeBlock, Expression, isConstantExpression, Model, ReferenceExpression } from '@dnd-language/index.js';
+import {
+    Code,
+    CodeBlock,
+    ConditionalBlock,
+    Expression,
+    FunctionCall,
+    FunctionDeclaration,
+    isBoolExpression,
+    isBoolVal,
+    isFunctionCall,
+    isGroupedExpression,
+    isIntExpression,
+    isIntToBoolExpression,
+    isIntVal,
+    isObjectDeclaration,
+    isRefChain,
+    isStringVal,
+    RefChain,
+    ReturnStatement,
+    VariableDeclaration,
+} from '@dnd-language/index.js';
 import { Injectable } from '@nestjs/common';
+
+type RuntimeScope = Record<string, any>;
+
+class ReturnSignal {
+    constructor(public readonly value: any) {}
+}
 
 @Injectable()
 export class LangiumInterpreterService {
 
-    evaluateExpression(worldState: any, expression: Expression): any {
+    private readonly predefinedFunctions: Record<string, (...args: any[]) => any> = {
+        randomfv: (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min,
+    };
 
-        if(isConstantExpression(expression)){
-            if(expression.$type === "IntVal" || expression.$type === "BoolVal" || expression.$type === "StringVal"){
-                return expression.val;
-            }
-
-            if(expression.$type === "ObjectDeclaration"){
-                return expression.variables.reduce((obj: any, variable) => {
-                    obj[variable.target!] = this.evaluateExpression(worldState, variable.value!);
-                    return obj;
-                }, {});
+    evaluateExpression(scope: RuntimeScope, expression: Expression): any {
+        if (isIntVal(expression)) {
+            return expression.isNegative ? -expression.val : expression.val;
+        }
+        if (isBoolVal(expression)) {
+            return expression.isNegated ? !expression.val : expression.val;
+        }
+        if (isStringVal(expression)) {
+            return expression.val;
+        }
+        if (isObjectDeclaration(expression)) {
+            return expression.variables.reduce((obj: RuntimeScope, v) => {
+                const name = v.target ?? v.name ?? '';
+                obj[name] = v.value ? this.evaluateExpression(scope, v.value) : undefined;
+                return obj;
+            }, {});
+        }
+        if (isGroupedExpression(expression)) {
+            return this.evaluateExpression(scope, expression.exp);
+        }
+        if (isIntExpression(expression)) {
+            const l = this.evaluateExpression(scope, expression.left);
+            const r = this.evaluateExpression(scope, expression.right);
+            switch (expression.operator) {
+                case '+': return l + r;
+                case '-': return l - r;
+                case '*': return l * r;
+                case '/': return r !== 0 ? l / r : 0;
             }
         }
-
-        if(expression.$type === "GroupedExpression"){
-            return `(${this.evaluateExpression(worldState, expression.exp)})`;
-        } 
-    }
-
-    evaluateReference(worldState: any, expression: ReferenceExpression): any {
-        if(expression.$type === "RefChain"){
-            return expression.first;
-            //TODO
+        if (isIntToBoolExpression(expression)) {
+            const l = this.evaluateExpression(scope, expression.left);
+            const r = this.evaluateExpression(scope, expression.right);
+            switch (expression.operator) {
+                case '==': return l === r;
+                case '!=': return l !== r;
+                case '<':  return l < r;
+                case '>':  return l > r;
+                case '<=': return l <= r;
+                case '>=': return l >= r;
+            }
         }
+        if (isBoolExpression(expression)) {
+            const l = this.evaluateExpression(scope, expression.left);
+            if (expression.operator === 'and') return l && this.evaluateExpression(scope, expression.right);
+            if (expression.operator === 'or')  return l || this.evaluateExpression(scope, expression.right);
+        }
+        if (isRefChain(expression)) {
+            return this.evaluateRefChain(scope, expression);
+        }
+        if (isFunctionCall(expression)) {
+            return this.executeFunctionCall(scope, expression);
+        }
+        throw new Error(`Unhandled expression type: ${expression.$type}`);
     }
 
-    
-    callFunction(params: callFunctionParams): any {
-
+    private evaluateRefChain(scope: RuntimeScope, chain: RefChain): any {
+        const first = chain.first;
+        if (first.$type === 'VariableRefItem') {
+            const decl = first.val.val.ref;
+            const name = decl?.target ?? decl?.name;
+            if (name === undefined) throw new Error(`Unresolved variable ref: ${first.val.val.$refText}`);
+            return scope[name];
+        }
+        // TODO: location/quest/event chain traversal
+        throw new Error(`Unhandled RefChainStart type: ${first.$type}`);
     }
 
-    runCodeBlock(worldState: any, codeBlock: CodeBlock): any {
-        codeBlock.code.forEach(code => {
-            this.runCode(worldState, code);
+    executeFunctionCall(scope: RuntimeScope, call: FunctionCall): any {
+        const args = call.params.map(p => this.evaluateExpression(scope, p));
+
+        if (call.predefined) {
+            const fn = this.predefinedFunctions[call.predefinedTarget!];
+            if (!fn) throw new Error(`Unknown predefined function: ${call.predefinedTarget}`);
+            return fn(...args);
+        }
+
+        const decl = call.target?.val.ref;
+        if (!decl) throw new Error(`Unresolved function ref: ${call.target?.val.$refText}`);
+        return this.callFunctionDecl(scope, decl, args);
+    }
+
+    private callFunctionDecl(callerScope: RuntimeScope, decl: FunctionDeclaration, args: any[]): any {
+        const localScope: RuntimeScope = Object.create(callerScope);
+
+        decl.params.forEach((param, i) => {
+            const name = param.name ?? param.target ?? '';
+            localScope[name] = args[i];
         });
+
+        if (!decl.codeBlock) return undefined;
+
+        const result = this.runCodeBlock(localScope, decl.codeBlock);
+        return result instanceof ReturnSignal ? result.value : undefined;
     }
 
-    runCode(worldState: any, code: Code): any {
-        if(code.$type === "FunctionCall"){
-            return this.callFunction({
-                worldState: worldState,
-                target: code.target!.val.ref!.name,
-                type: "name"
-            });
+    runCodeBlock(scope: RuntimeScope, codeBlock: CodeBlock): ReturnSignal | undefined {
+        for (const code of codeBlock.code) {
+            const result = this.runCode(scope, code);
+            if (result instanceof ReturnSignal) return result;
         }
+        return undefined;
+    }
 
-        if(code.$type === "VariableAssignment"){
-            const value = this.evaluateExpression(worldState, code.value);
-            const variableName = code.target.val.ref!.name!;
-            worldState[variableName] = value;
+    runCode(scope: RuntimeScope, code: Code): ReturnSignal | undefined {
+        switch (code.$type) {
+            case 'VariableDeclaration': {
+                const c = code as VariableDeclaration;
+                const name = c.target ?? '';
+                scope[name] = c.value ? this.evaluateExpression(scope, c.value) : undefined;
+                break;
+            }
+            case 'VariableAssignment': {
+                const decl = code.target.val.ref;
+                const name = decl?.target ?? decl?.name ?? '';
+                scope[name] = this.evaluateExpression(scope, code.value);
+                break;
+            }
+            case 'FunctionCall': {
+                const c = code as unknown as FunctionCall;
+                this.executeFunctionCall(scope, c);
+                break;
+            }
+            case 'ReturnStatement': {
+                const c = code as unknown as ReturnStatement;
+                const value = c.returnValue ? this.evaluateExpression(scope, c.returnValue) : undefined;
+                return new ReturnSignal(value);
+            }
+            case 'ConditionalBlock': {
+                const c = code as unknown as ConditionalBlock;
+                const condition = this.evaluateExpression(scope, c.condition);
+                if (condition) {
+                    for (const block of c.body) {
+                        const result = this.runCodeBlock(scope, block);
+                        if (result instanceof ReturnSignal) return result;
+                    }
+                }
+                break;
+            }
+            case 'EventTrigger': {
+                // TODO: dispatch event execution
+                break;
+            }
         }
+        return undefined;
     }
 }
-
-type callFunctionParams = {
-        worldState: any,
-        target: string,
-        type?: "reference" | "name",
-    }
